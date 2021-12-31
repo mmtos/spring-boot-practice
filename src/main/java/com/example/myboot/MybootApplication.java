c6123e48bb6fc4ff046c53f7b58303fb6cf47ed8
mmtos <js223kr92@naver.com>
2021-12-31 14:45:18 +0900

    스프링 부트에서 자동설정하는 빈들의 목록 및 개수 확인

--- src/main/java/com/example/myboot/MybootApplication.java
+++ src/main/java/com/example/myboot/MybootApplication.java
@@ -1,7 +1,12 @@
 package com.example.myboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MybootApplication {
@@ -10,4 +15,16 @@ public class MybootApplication {
 		SpringApplication.run(MybootApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return arg -> {
+			System.out.println("Let's inspect beans!");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			System.out.println("===== 스프링 빈 개수 : " + beanNames.length + " =====");
+			Arrays.sort(beanNames);
+			for(String beanName : beanNames){
+				System.out.println(beanName);
+			}
+		};
+	}
 }
